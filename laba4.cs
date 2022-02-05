338a48ff8e83cc51097c5ae78cf9e24962b24bcb
lappely <kirillgnapov@gmail.com>
2022-02-06 00:34:28 +0300

    laba4 fixed

--- laba4.cs
+++ laba4.cs
@@ -2,36 +2,28 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    var enterprise = new Enterprise(1200, 2400, 1200, 15000000);
-    Console.WriteLine(enterprise);
-    Console.WriteLine($"The Earnings without a loan:          {enterprise.earningsWithoutLoan()}");
-    Console.WriteLine($"The Earnings without a salary outlay: {enterprise.earningsWithoutSalary()}");
-    Console.WriteLine($"The Earnings without a taxes:         {enterprise.earningsWithoutTaxes()}");
-    Console.WriteLine($"The Earnings without any outlay:      {enterprise.earningsWithoutOutlay()}");
+    const int    earnings   = 2500;
+    const int    sumOfLoans = 1000;
+    const int    salaries   = 200;
+    const double taxes      = 0.24;
+    const double loan       = 0.12;
+
+    Console.WriteLine($"The Earnings without a loan:          {getEarnings(earnings, loan, sumOfLoans)}");
+    Console.WriteLine($"The Earnings without a salary outlay: {getEarnings(earnings, salaries)}");
+    Console.WriteLine($"The Earnings without a taxes:         {getEarnings(earnings, taxes)}");
+    Console.WriteLine($"The Earnings without any outlay:      {getEarnings(earnings)}");
   }
-}
 
-class Enterprise {
-  public int AllOutlay    { get; private set; }
-  public int TaxOutlay    { get; set; }
-  public int SalaryOutlay { get; set; }
-  public int LoanOutlay   { get; set; }
-  public int Earnings     { get; set; }
+  static int getEarnings(int earnings) =>
+    earnings;
 
-  public Enterprise(int tax, int loan, int salary, int earnings) =>
-    (AllOutlay, TaxOutlay, LoanOutlay, SalaryOutlay, Earnings) = 
-      (tax+salary+loan, tax, loan, salary, earnings);
+  static double getEarnings(int earnings, double taxes) =>
+    earnings - (earnings * taxes);
 
-  public int earningsWithoutOutlay() => Earnings - AllOutlay;
-  public int earningsWithoutTaxes()  => Earnings - TaxOutlay;
-  public int earningsWithoutSalary() => Earnings - SalaryOutlay;
-  public int earningsWithoutLoan()   => Earnings - LoanOutlay;
+  static int getEarnings(int earnings, int salaries) =>
+    earnings - salaries;
 
-  public override string ToString() =>
-    "---------------summary--------------------\n"       +
-    $"The Earnings of Enterprise is: {Earnings}\n"     +
-    $"The sum of tax is:             {TaxOutlay}\n"    +
-    $"The salary outlay is:          {SalaryOutlay}\n" +
-    $"The loan outlay is:            {LoanOutlay}\n"   +
-    $"The sum of all outlays is:     {AllOutlay}\n"       ; 
+  static double getEarnings(int earnings, double loan, int sumOfLoan) =>
+    earnings - (loan * sumOfLoan);
+
 }
