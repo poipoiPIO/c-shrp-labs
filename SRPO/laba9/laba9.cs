20c1fa72b1156bc3a28a0230fbcdb063f77caf1d
Lappely <kirillgnapov@gmail.com>
2022-03-18 00:07:54 +0300

    laba9 solved

---
+++ SRPO/laba9/laba9.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+class Program {
+  static void Main() {
+    var input = File.ReadAllText("./file-to-read.txt");
+    var solve = prettiefyValues(calculateForArray(castStringNumsToInt(input)));
+    File.WriteAllText("./file-to-write.txt", solve);
+  }
+  
+  static IEnumerable<double> castStringNumsToInt(string input) =>
+    input.Split(" ")
+         .Select(str => Double.Parse(str));
+
+  static (double, double) calculateFormula(double x) =>
+    (x, ((1 - Math.Pow(x, 2)) / (1 + Math.Pow(x, 4))));
+
+  static IEnumerable<(double, double)> calculateForArray(IEnumerable<double> array) =>
+    array.Select(x => calculateFormula(x));
+
+  static string prettiefyValues(IEnumerable<(double, double)> results) =>
+    string.Join("", results.Select(res => prettiefyValue(res)));
+
+  static string prettiefyValue((double, double) result) =>
+    $"x = {result.Item1} | y = {result.Item2}\n"; 
+}
+
